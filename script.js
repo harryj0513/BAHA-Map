6185ba7404bead933862a2deef37a191f746d68f
harryj0513 <harryj0513@berkeley.edu>
2025-10-27 10:23:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGFycnlqMDUxMyIsImEiOiJjbWg5ZDBoNzYwOGJrMmlvbXkyd29lNjNhIn0.JrUyntoDtePiPloFx4iXKQ';
+
+const map = new mapboxgl.Map({
+  container:'map', 
+  style:'mapbox://styles/harryj0513/cmh9ei12900q101sr4yhx5jkv',
+  center: [-122.27, 37.87],
+  zoom: 9,
+});
